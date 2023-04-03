db8172805624f91cf7eb7a9a5ef1c5cbea0cc27f
Bruna Almeida <99504245+brualmeidaj@users.noreply.github.com>
2023-04-02 22:06:18 -0300

    segundaparte2fundamentos

--- exercicios-js/fundamentos/parNomeValor.js
+++ exercicios-js/fundamentos/parNomeValor.js
@@ -1,1 +1,21 @@
 //par nome/valor
+const saudacao = 'Opa' //contexto léxico 1 (local fisico aonde o codigo foi definido)
+
+function exec() {
+    const saudacao = 'Falaaaa' // contexto léxico 2
+    return saudacao
+}
+
+// Objetos são grupos aninhados de pares nome/valor
+const cliente ={
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: 'Rua Muito Legal',
+        numero: 123
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
